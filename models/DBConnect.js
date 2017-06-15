2a3c74b4bbd73e5c725ddbc9e2d4a88f10f49c51
Shawn Springstead <stang90@gmail.com>
2017-06-15 16:18:09 -0400

    set enviroment vars

--- models/DBConnect.js
+++ models/DBConnect.js
@@ -1,11 +1,18 @@
 var mongoose = require('mongoose');
-var config = require('../config');
+var fs = require('fs');
+
+var config = null;
+
+if (fs.existsSync('config.js')) {
+   config = require('../config.js'); 
+}
+else{
+	config = process.env;
+}
 
 var dbuser = process.env.dbuser || config.dbuser;
 var dbpass = process.env.dbpass || config.dbpass;
 
-console.log('HERE!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!V');
-console.log(process.env);
 
 mongoose.Promise = global.Promise; // use native mongoose promises﻿
 
