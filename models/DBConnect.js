f9348708dccf567cc96b47c65c38b3f0c7ff4740
Shawn Springstead <stang90@gmail.com>
2017-06-21 16:25:21 -0400

    mlab reccommended options

--- models/DBConnect.js
+++ models/DBConnect.js
@@ -13,11 +13,23 @@ else{
 var dbuser = process.env.dbuser || config.dbuser;
 var dbpass = process.env.dbpass || config.dbpass;
 
-
 mongoose.Promise = global.Promise; // use native mongoose promises﻿
 
+/* 
+ * Mongoose by default sets the auto_reconnect option to true.
+ * We recommend setting socket options at both the server and replica set level.
+ * We recommend a 30 second connection timeout because it allows for 
+ * plenty of time in most operating environments.
+ */
+var options = { server: { socketOptions: { keepAlive: 300000, connectTimeoutMS: 30000 } }, 
+                replset: { socketOptions: { keepAlive: 300000, connectTimeoutMS : 30000 } } };   
+
 //conntect to database
-mongoose.connect( `mongodb://${dbuser}:${dbpass}@ds149551.mlab.com:49551/big_mac_index` );
+mongoose.connect( `mongodb://${dbuser}:${dbpass}@ds149551.mlab.com:49551/big_mac_index`, options );
+
+var conn = mongoose.connection;
+
+conn.on('error', console.error.bind(console, 'connection error:'));
 
 module.exports = {
   mongoose: mongoose
