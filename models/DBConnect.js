6a71017dda3a9c1aeee9f0630f6b512089aca208
Shawn Springstead <stang90@gmail.com>
2017-06-21 16:10:38 -0400

    change mongoose version

--- models/DBConnect.js
+++ models/DBConnect.js
@@ -13,17 +13,11 @@ else{
 var dbuser = process.env.dbuser || config.dbuser;
 var dbpass = process.env.dbpass || config.dbpass;
 
+
 mongoose.Promise = global.Promise; // use native mongoose promises﻿
 
 //conntect to database
-mongoose.connect( `mongodb://${dbuser}:${dbpass}@ds149551.mlab.com:49551/big_mac_index`)
-.then(() => {
-	return true;
-})
-.catch(err => {
-	console.error('App starting error:', err.stack);
-	process.exit(1);
-})
+mongoose.connect( `mongodb://${dbuser}:${dbpass}@ds149551.mlab.com:49551/big_mac_index` );
 
 module.exports = {
   mongoose: mongoose
